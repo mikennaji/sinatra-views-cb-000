5f895a625e473609c14b81eacc7b6fbe032e92d1
Michael Nnaji <mnnaji3@uwo.ca>
2020-11-29 17:08:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,11 +3,11 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
-		erb :index 
-	end 
+		erb :index
+	end
 
-	get "/info" do 
+	get "/info" do
 		"Testing the info page"
-	end 
+	end
 	
 end
